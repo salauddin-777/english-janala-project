8bfc6e965bcb662ca4f1baeef848e0bac96d805d
salauddin-777 <mdsalauddinmina777@gmail.com>
2025-09-04 22:40:58 +0600

    Modal Added when someone click into info button on a card

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ const displayLevelWord = (words) => {
           <p class="font-semibold ">"${word.meaning? word.meaning : 'শব্দ পাওয়া যায় নি '} / ${word.pronunciation ? word.pronunciation : 'শব্দ পাওয়া যায় নি ' }"</p>
           <div class="text-2xl font-medium f-bangla mb-6"></div>
           <div class="flex justify-between items-center ">
-            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info "></i></button>
+            <button onclick="my_modal_5.showModal()" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info "></i></button>
             <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high "></i></button>
           </div>
         </div>
